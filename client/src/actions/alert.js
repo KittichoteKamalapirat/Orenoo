b07b0fa991f5fc00372815e3f2866ed9923b2627
Kittichote Kamalapirat <shane@Kittichotes-MacBook-Air.local>
2021-12-10 10:40:04 +0700

    edit uuid import

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,12 +1,15 @@
-import { SET_ALERT, REMOVE_ALERT } from './types';
-import uuid from 'uuid';
+import { SET_ALERT, REMOVE_ALERT } from "./types";
+// import uuid from 'uuid';
+import uuid from "uuid/v4";
 
-export const setAlert = (msg, alertType, timeout = 3000) => dispatch => {
-  const id = uuid.v4();
-  dispatch({
-    type: SET_ALERT,
-    payload: { msg, alertType, id }
-  });
+export const setAlert =
+  (msg, alertType, timeout = 3000) =>
+  (dispatch) => {
+    const id = uuid.v4();
+    dispatch({
+      type: SET_ALERT,
+      payload: { msg, alertType, id },
+    });
 
-  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
-};
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
+  };
